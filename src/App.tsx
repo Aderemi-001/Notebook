31a4392b3ca85d878233a469086a302c66e709fa
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-20 21:40:38 +0200

    [dyad] Re-enabled `QueryClientProvider` in `App.tsx` to resolve the "No QueryClient set" error. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -14,115 +14,114 @@ import TakeExam from "./pages/TakeExam";
 import PastExams from "./pages/PastExams";
 import AuthLayout from "./layouts/AuthLayout";
 import { Toaster } from "@/components/ui/sonner";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Temporarily commented out
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Re-enabled
 import React from "react";
 
-// const queryClient = new QueryClient(); // Temporarily commented out
+const queryClient = new QueryClient(); // Re-enabled
 
 function App() {
   return (
-    // <QueryClientProvider client={queryClient}> // Temporarily removed
-    <div>
-      {/* Replaced with a simple div */}
-      <BrowserRouter>
-        <React.Fragment>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <AuthLayout>
-                  <Index />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/create"
-              element={
-                <AuthLayout>
-                  <CreateSet />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/sets/:setId"
-              element={
-                <AuthLayout>
-                  <StudySetDetail />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/sets/:setId/study"
-              element={
-                <AuthLayout>
-                  <StudyMode />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/sets/:setId/edit"
-              element={
-                <AuthLayout>
-                  <EditSet />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <AuthLayout>
-                  <Profile />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/constellation"
-              element={
-                <AuthLayout>
-                  <CognitiveConstellation />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/explore"
-              element={
-                <AuthLayout>
-                  <ExplorePublicSets />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/generate-exam"
-              element={
-                <AuthLayout>
-                  <GenerateExam />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/exams/:examId"
-              element={
-                <AuthLayout>
-                  <TakeExam />
-                </AuthLayout>
-              }
-            />
-            <Route
-              path="/past-exams"
-              element={
-                <AuthLayout>
-                  <PastExams />
-                </AuthLayout>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Toaster richColors />
-        </React.Fragment>
-      </BrowserRouter>
-    </div>
-    // </QueryClientProvider> // Temporarily removed
+    <QueryClientProvider client={queryClient}> {/* Re-enabled */}
+      <div>
+        <BrowserRouter>
+          <React.Fragment>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <AuthLayout>
+                    <Index />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/create"
+                element={
+                  <AuthLayout>
+                    <CreateSet />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/sets/:setId"
+                element={
+                  <AuthLayout>
+                    <StudySetDetail />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/sets/:setId/study"
+                element={
+                  <AuthLayout>
+                    <StudyMode />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/sets/:setId/edit"
+                element={
+                  <AuthLayout>
+                    <EditSet />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <AuthLayout>
+                    <Profile />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/constellation"
+                element={
+                  <AuthLayout>
+                    <CognitiveConstellation />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/explore"
+                element={
+                  <AuthLayout>
+                    <ExplorePublicSets />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/generate-exam"
+                element={
+                  <AuthLayout>
+                    <GenerateExam />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/exams/:examId"
+                element={
+                  <AuthLayout>
+                    <TakeExam />
+                  </AuthLayout>
+                }
+              />
+              <Route
+                path="/past-exams"
+                element={
+                  <AuthLayout>
+                    <PastExams />
+                  </AuthLayout>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Toaster richColors />
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider> {/* Re-enabled */}
   );
 }
 
