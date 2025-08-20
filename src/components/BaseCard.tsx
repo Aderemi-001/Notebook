8716499dcee211f651330f7e0ccedb7f427308fa
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-20 18:44:04 +0200

    [dyad] wrote 2 file(s)

---
+++ src/components/BaseCard.tsx
@@ -0,0 +1,3 @@
+import { Card, CardProps, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+
+export { Card, CardProps, CardHeader, CardTitle, CardDescription, CardContent, CardFooter };
