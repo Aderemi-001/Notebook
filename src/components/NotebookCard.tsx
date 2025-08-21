78976f8109418030e7d3c84aca1af39bccf45447
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-21 02:44:06 +0200

    [dyad] wrote 2 file(s)

--- src/components/NotebookCard.tsx
+++ src/components/NotebookCard.tsx
@@ -13,8 +13,7 @@ const NotebookCard = React.forwardRef<HTMLDivElement, NotebookCardProps>(
       className={cn(
         "relative shadow-lg hover:shadow-xl transition-shadow duration-300",
         "border border-gray-200 rounded-md",
-        "before:content-[''] before:absolute before:left-4 before:top-0 before:bottom-0 before:w-px before:bg-red-400", // Red margin line
-        // Removed "pl-10" from here
+        "before:content-[''] before:absolute before:left-4 before:top-0 before:bottom-0 before:w-px before:bg-[--notebook-line-color]", // Use CSS variable
         className
       )}
       {...props}
@@ -22,7 +21,7 @@ const NotebookCard = React.forwardRef<HTMLDivElement, NotebookCardProps>(
       {/* Hole punch effect */}
       <div className="absolute left-0 top-0 bottom-0 w-4 flex flex-col justify-around py-2">
         {Array.from({ length: 5 }).map((_, i) => (
-          <div key={i} className="w-2 h-2 bg-gray-300 rounded-full mx-auto"></div>
+          <div key={i} className="w-2 h-2 rounded-full mx-auto bg-[--notebook-hole-color]"></div> {/* Use CSS variable */}
         ))}
       </div>
       {children}
