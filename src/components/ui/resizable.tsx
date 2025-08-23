e00b7696ff9dbbf0c61db0ef7e7003b06854af9b
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-23 12:04:11 +0200

    [dyad] Fixed a TypeScript error in ResizableHandle by updating its prop type to correctly include the ref. - wrote 2 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,49 +1,44 @@
-"use client"
-
-import * as React from "react"
-import {
-  PanelGroup,
-  PanelGroupProps,
-  Panel,
-  PanelProps,
-  PanelResizeHandle,
-} from "react-resizable-panels"
-
-import { cn } from "@/lib/utils"
+import { GripVertical } from "lucide-react";
+import * as ResizablePrimitive from "react-resizable-panels";
+import * as React from "react";
+import { cn } "@/lib/utils";
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: PanelGroupProps) => (
-  <PanelGroup
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
+  <ResizablePrimitive.PanelGroup
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
-)
+);
 
-const ResizablePanel = ({
-  className,
-  ...props
-}: PanelProps) => (
-  <Panel className={cn(className)} {...props} />
-)
+const ResizablePanel = ResizablePrimitive.Panel;
 
 const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof PanelResizeHandle>,
-  React.ComponentPropsWithoutRef<typeof PanelResizeHandle>
->(({ className, ...props }, ref) => (
-  <PanelResizeHandle
-    ref={ref as any} // Cast ref to any to bypass the type error
+  React.ElementRef<typeof ResizablePrimitive.PanelResizeHandle>,
+  React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & { // Changed to ComponentProps
+    withHandle?: boolean;
+  }
+>(({ withHandle, className, ...props }, ref) => (
+  <ResizablePrimitive.PanelResizeHandle
+    ref={ref}
     className={cn(
-      "flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
     )}
     {...props}
-  />
-))
-ResizableHandle.displayName = "ResizableHandle"
+  >
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+        <GripVertical className="h-2.5 w-2.5" />
+      </div>
+    )}
+  </ResizablePrimitive.PanelResizeHandle>
+));
+ResizableHandle.displayName = "ResizableHandle";
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
