cc3cda385c03c2850918a7efeb4958f2c6598dc1
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-23 16:59:40 +0200

    [dyad] Fixed React Router context error by wrapping with BrowserRouter and AuthProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AppContent from "@/components/AppContent"; // Import the new AppContent component
+import AppContent from "@/components/AppContent";
 import * as React from "react";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import { AuthProvider } from "@/hooks/useAuth"; // Import AuthProvider
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppContent />
+      <BrowserRouter> {/* Wrap with BrowserRouter */}
+        <AuthProvider> {/* Wrap with AuthProvider */}
+          <AppContent />
+        </AuthProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
