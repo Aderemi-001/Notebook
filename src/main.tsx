4236539e94c2524b9ba8b6d27f2449e1ee036bc9
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-24 19:10:23 +0200

    [dyad] Added Toaster component for displaying notifications - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import { Toaster } from "sonner"; // Import Toaster
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" attribute="class">
     <App />
+    <Toaster /> {/* Add the Toaster component here */}
   </ThemeProvider>
 );
