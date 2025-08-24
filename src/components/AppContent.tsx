9465d657572cb3eedf083620dbb16787ca642d07
[dyad] <anthony.adesanmi400@gmail.com>
2025-08-24 09:14:40 +0200

    [dyad] wrote 2 file(s), renamed 1 file(s), deleted 7 file(s)

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -4,14 +4,33 @@ import About from "@/pages/About";
 import Contact from "@/pages/Contact";
 import Login from "@/pages/Login";
 import StudySetDetail from "@/pages/StudySetDetail";
-import StudySetForm from "@/pages/StudySetForm";
-import StudyPage from "@/pages/StudyPage";
-import NotesPage from "@/pages/NotesPage";
-import NoteDetail from "@/pages/NoteDetail";
-import ProfilePage from "@/pages/ProfilePage";
-import StudySetGroupForm from "@/pages/StudySetGroupForm";
-import StudySetGroupDetail from "@/pages/StudySetGroupDetail";
-import ExplorePublicSets from "@/pages/ExplorePublicSets"; // Import the new component
+import CreateStudySet from "@/pages/CreateStudySet"; // Renamed and imported
+import EditSet from "@/pages/EditSet"; // Imported
+import StudyMode from "@/pages/StudyMode"; // Imported
+import NotesIndex from "@/pages/NotesIndex"; // Imported
+import CreateNote from "@/pages/CreateNote"; // Imported
+import EditNote from "@/pages/EditNote"; // Imported
+import Profile from "@/pages/Profile"; // Imported
+import GroupsIndex from "@/pages/GroupsIndex"; // Imported
+import CreateGroup from "@/pages/CreateGroup"; // Imported
+import GroupDetail from "@/pages/GroupDetail"; // Imported
+import EditGroup from "@/pages/EditGroup"; // Imported
+import ExplorePublicSets from "@/pages/ExplorePublicSets"; // Imported
+import DailyReview from "@/pages/DailyReview"; // Imported
+import Settings from "@/pages/Settings"; // Imported
+import Statistics from "@/pages/Statistics"; // Imported
+import CognitiveConstellation from "@/pages/CognitiveConstellation"; // Imported
+import Collaborations from "@/pages/Collaborations"; // Imported
+import ExamsIndex from "@/pages/ExamsIndex"; // Imported
+import GenerateExam from "@/pages/GenerateExam"; // Imported
+import PastExams from "@/pages/PastExams"; // Imported
+import TakeExam from "@/pages/TakeExam"; // Imported
+import EssayIndex from "@/pages/EssayIndex"; // Imported
+import GenerateEssayQuestions from "@/pages/GenerateEssayQuestions"; // Imported
+import PastEssayQuestions from "@/pages/PastEssayQuestions"; // Imported
+import EssayPractice from "@/pages/EssayPractice"; // Imported
+import PrivacyPolicy from "@/pages/PrivacyPolicy"; // Imported
+import NotFound from "@/pages/NotFound"; // Imported
 
 const AppContent = () => {
   return (
@@ -19,19 +38,47 @@ const AppContent = () => {
       <Route path="/" element={<Index />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/privacy" element={<PrivacyPolicy />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/dashboard" element={<Statistics />} />
+      <Route path="/daily-review" element={<DailyReview />} />
+      <Route path="/constellation" element={<CognitiveConstellation />} />
+      <Route path="/collaborations" element={<Collaborations />} />
+      <Route path="/explore-public-sets" element={<ExplorePublicSets />} />
+
+      {/* Study Sets */}
+      <Route path="/create" element={<CreateStudySet />} />
       <Route path="/sets/:setId" element={<StudySetDetail />} />
-      <Route path="/sets/new" element={<StudySetForm />} />
-      <Route path="/sets/:setId/edit" element={<StudySetForm />} />
-      <Route path="/sets/:setId/study" element={<StudyPage />} />
-      <Route path="/notes" element={<NotesPage />} />
-      <Route path="/notes/new" element={<NoteDetail />} />
-      <Route path="/notes/:noteId" element={<NoteDetail />} />
-      <Route path="/groups/new" element={<StudySetGroupForm />} />
-      <Route path="/groups/:groupId" element={<StudySetGroupDetail />} />
-      <Route path="/groups/:groupId/edit" element={<StudySetGroupForm />} />
-      <Route path="/explore-public-sets" element={<ExplorePublicSets />} /> {/* Add the new route */}
+      <Route path="/sets/:setId/edit" element={<EditSet />} />
+      <Route path="/sets/:setId/study" element={<StudyMode />} />
+
+      {/* Notes */}
+      <Route path="/notes" element={<NotesIndex />} />
+      <Route path="/create-note" element={<CreateNote />} />
+      <Route path="/notes/:noteId/edit" element={<EditNote />} />
+
+      {/* Study Set Groups */}
+      <Route path="/groups" element={<GroupsIndex />} />
+      <Route path="/groups/create" element={<CreateGroup />} />
+      <Route path="/groups/:groupId" element={<GroupDetail />} />
+      <Route path="/groups/:groupId/edit" element={<EditGroup />} />
+
+      {/* Exams */}
+      <Route path="/exams" element={<ExamsIndex />} />
+      <Route path="/generate-exam" element={<GenerateExam />} />
+      <Route path="/past-exams" element={<PastExams />} />
+      <Route path="/exams/:examId" element={<TakeExam />} />
+
+      {/* Essays */}
+      <Route path="/essays" element={<EssayIndex />} />
+      <Route path="/generate-essay-questions" element={<GenerateEssayQuestions />} />
+      <Route path="/past-essay-questions" element={<PastEssayQuestions />} />
+      <Route path="/essay-practice/:questionId" element={<EssayPractice />} />
+
+      {/* Catch-all for 404 */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
